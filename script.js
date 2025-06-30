5499c2f13a59b83ed13260d9d205181b454802d9
zaidoi <zaidbadgujar7@gmail.com>
2025-06-30 20:38:39 +0530

    ParseInt with radix

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// ParseInt()
+// ParseInt() With Radix
 
 function convertToInteger(str){
-    return parseInt(str)
+    return parseInt(str,2)
 }
 
-convertToInteger("56")
+convertToInteger("10011")
+
+// first alaphabet not a number
+// 2 binary , 10 decimal , 16 for hex
