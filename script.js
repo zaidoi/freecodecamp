7f49d08a8232866f8a46f20a7f345e39677a16c5
zaidoi <zaidbadgujar7@gmail.com>
2025-07-01 22:45:09 +0530

    all types of destructing

--- script.js
+++ script.js
@@ -28,3 +28,27 @@ const LOCAL_FORECAST = {
   // or swapping
    let a=4 , b=5;
    [a,b]=[b,a]// 4 , 5
+
+   // rest destructing
+   function removeFirstTwo(list) {
+    const [a,b,...arr] = list
+      return arr;
+    }
+    
+    const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+    // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+    const stats = {
+        max: 56.78,
+        standard_deviation: 4.34,
+        median: 34.54,
+        mode: 23.87,
+        min: -0.75,
+        average: 35.85
+      };
+      
+      // Only change code below this line
+      const half = ({ max, min }) => (max + min) / 2.0;
+      // Only change code above this line
