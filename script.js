f212979cbf964168c554f67f567b5cfd7615ed31
zaidoi <zaidbadgujar7@gmail.com>
2025-07-01 20:45:39 +0530

    destructuring of array and object and nested

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-// spread and rest
-//spread
-let arr1 = [1,3,5]
-let arr3 = [...arr1]
-console.log(arr3); // [1,3,5]
+// destructing object
 
-// rest
-
-function rest (...args){
-    return args
+const value = {
+    name :"zaid",
+    surname:"badgujar"
 }
 
-rest("ss0",1,4)  // takes multiple argument and makes array
+const { name: firstName , surname:lastName} = value // firstName = zaid lastName = badgujar
+
+// nested destructing
+
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const {today :{ low: lowToday, high: highToday}} = LOCAL_FORECAST
+  
+
+  // array destructuring 
+
+  const arr = [1,2,3,4,5];
+  [a, b] = arr // 12 or comma ,, for differnt val
+
+  // or swapping
+   let a=4 , b=5;
+   [a,b]=[b,a]// 4 , 5
