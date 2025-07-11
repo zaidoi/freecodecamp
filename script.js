7770f7bc728ccfa0c41690bb35b8a4c8c7ea7dda
zaidoi <zaidbadgujar7@gmail.com>
2025-07-11 18:52:21 +0530

    Filter function made

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-Array.prototype.myMap = function(callback) {
-    const newArray = [];
-    // Only change code below this line
-  for(let i = 0; i < this.length; i++){
-    newArray.push(callback(this[i],i,this))
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+for(let i = 0; this.length; i++){
+  if(Boolean(callback(this[i],i,this)) === true){
+    newArray.push(this[i])
   }
-    // Only change code above this line
-    return newArray;
-  };
+}
+  // Only change code above this line
+  return newArray;
+};
