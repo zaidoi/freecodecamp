d0819264f5af0c8742a18eae626525f6fd442cd9
zaidoi <zaidbadgujar7@gmail.com>
2025-07-12 23:57:29 +0530

    diff two arrays

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function sumAll(arr) {
-  const sortFirst = arr.sort((a,b) => a-b);
-  let firstElement = sortFirst[0]
-  let lastElement = sortFirst[1]
-  let sumArr = 0
- for(let i = firstElement; i <= lastElement; i++){
-     sumArr = sumArr + i
- }
-  
-  console.log(sumArr);
-  
+function diffArray(arr1, arr2) {
+  const newArr = [];
+for(let i = 0; i < arr1.length; i++){
+  if(!arr2.includes(arr1[i])){
+    newArr.push(arr1[i])
+  }
+}
+for(let i = 0; i < arr2.length; i++){
+  if(!arr1.includes(arr2[i])){
+    newArr.push(arr2[i])
+  }
 }
 
-sumAll([10, 5]);
+
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
