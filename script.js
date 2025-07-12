9a3bc2f25174ebe8864572d68ad4569f80208ac3
zaidoi <zaidbadgujar7@gmail.com>
2025-07-12 23:07:55 +0530

    sum all numbers in range

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function add(x) {
-  // Only change code below this line
-return function(y){
-  return function(z){
-    return x + y +z
-  }
+function sumAll(arr) {
+  const sortFirst = arr.sort((a,b) => a-b);
+  let firstElement = sortFirst[0]
+  let lastElement = sortFirst[1]
+  let sumArr = 0
+ for(let i = firstElement; i <= lastElement; i++){
+     sumArr = sumArr + i
+ }
+  
+  console.log(sumArr);
+  
 }
 
-  // Only change code above this line
-}
-
-add(10)(20)(30);
+sumAll([10, 5]);
