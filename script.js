7c1e895073e79bcec2ec21043d27bd54e1de4136
zaidoi <zaidbadgujar7@gmail.com>
2025-07-18 00:19:30 +0530

    value truthy check

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-function binaryAgent (str){
-  let binaryArr = str.split(" ");
-  let finalStr = ""
+function truthCheck(collection, pre) {
+  let finalVal = collection.every((el) => Boolean(el[pre]))
+  console.log(finalVal);
   
-  for(let i = 0; i < binaryArr.length; i++){
 
-    let charCode = parseInt(binaryArr[i],2);
-
-    finalStr += String.fromCharCode(charCode)
-
-  }
-return finalStr
-  
-  
 }
 
-binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111")
+truthCheck([{name: "Quincy", role: "Founder", isBot: true}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
