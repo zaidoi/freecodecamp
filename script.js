77193d14a894d0ed625630c37f6da4f637410c82
zaidoi <zaidbadgujar7@gmail.com>
2025-07-17 23:19:17 +0530

    binary to str

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function dropElements(arr, func) {
-  while(arr.length > 0){
-  if(func(arr[0])){
-    console.log(arr);
-    return arr
-  }else{
-    arr.shift()
+function binaryAgent (str){
+  let binaryArr = str.split(" ");
+  let finalStr = ""
+  
+  for(let i = 0; i < binaryArr.length; i++){
+
+    let charCode = parseInt(binaryArr[i],2);
+
+    finalStr += String.fromCharCode(charCode)
+
   }
-return []
+return finalStr
+  
+  
 }
-}
-dropElements([0, 1, 0,1], function(n) {return n === 1; });
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111")
