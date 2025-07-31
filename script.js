77bd645a4f816c936d7c002b2b0f2423753cfc7d
zaidoi <zaidbadgujar7@gmail.com>
2025-07-31 23:56:50 +0530

    Promise

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const userLeft = false
-const userWatchingCatMeme = false
-
-let watchTutorialCallback = new Promise((resolve,reject) => {
-  if(userLeft){
-    reject({
-      name:"User Left",
-      message: ':('
-    })
-  }else if(userWatchingCatMeme){
-    reject({
-      name:'User watching Cat Meme',
-      message: 'WevDevSimplied < Cat'
-    })
-  }else{
-  resolve('Thumbs up and Subscribe')
-  }
-})
-
-watchTutorialCallback.then((message) =>{
-  console.log(message);
-}).catch((message)=>{
-  console.log(message)
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Done'),1000)
 })
